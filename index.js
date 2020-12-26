b82be5390d85a4b2638e4696df9a7d0e2196921c
Yazid <yazidelg5@gmail.com>
2020-12-26 23:15:59 +0100

    3000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname,'assets')))
 app.set('view engine','ejs')
 app.set('views','views')
 
-app.listen(5050,() =>{
+app.listen(3000,() =>{
   console.log("server listening on port 3000")  
 })
 
