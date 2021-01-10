bd31ba20ea9fec0d662709197106015c54ecba70
ayman <aymankaddioui@gmail.com>
2021-01-10 22:08:11 +0100

    fix spring 3

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,6 @@ const router = require('express').Router()
 const Search = require('../controllers/search')
 const search = new Search();
 
-router.get('/seemore/', (req,res) => {
-    res.render('seemore')
-})
-
-
 router.use('/seemore/:id', (req,res) => {
     medecinId = req.params.id;
     search.seemore(medecinId,function (medinfo) {
